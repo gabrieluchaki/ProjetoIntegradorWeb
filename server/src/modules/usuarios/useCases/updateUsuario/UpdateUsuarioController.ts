222b4c86dfce0087a4273fbc87911fb5a02427cf
Vitor Meurer <meurervitor08@gmail.com>
2024-06-30 17:48:34 -0300

    update usuarios alterado

--- server/src/modules/usuarios/useCases/updateUsuario/UpdateUsuarioController.ts
+++ server/src/modules/usuarios/useCases/updateUsuario/UpdateUsuarioController.ts
@@ -5,12 +5,12 @@ import { UpdateUsuarioUseCase } from "./UpdateUsuarioUseCase";
 class UpdateUsuarioController {
   async handle (request: Request, response: Response): Promise<Response> {
     const { id } = request.params
-    const {nome, tipo, email, telefone} = request.body
+    const {nome, tipo, email, telefone, status, cpf, dataNascimento, genero, senha} = request.body
 
     const usuarioRepository = new UsuarioRepository()
     const updateUsuarioUseCase = new UpdateUsuarioUseCase(usuarioRepository)
 
-    const usuarioAtt = await updateUsuarioUseCase.execute({id, nome, tipo, email, telefone})
+    const usuarioAtt = await updateUsuarioUseCase.execute({id, nome, tipo, email, telefone, status, cpf, dataNascimento, genero, senha})
 
     return response.status(200).json({usuarioAtt})
   }
--- server/src/modules/usuarios/useCases/updateUsuario/UpdateUsuarioUseCase.ts
+++ server/src/modules/usuarios/useCases/updateUsuario/UpdateUsuarioUseCase.ts
@@ -6,8 +6,13 @@ interface IRequest {
   id: string
   nome: string
   tipo: string 
-  email: string
-  telefone: string 
+  email: string 
+  telefone: string // = 
+  status: string
+  cpf: string
+  dataNascimento: Date
+  genero: string
+  senha: string
 }
 
 const usuarioTipoSchema = z.union([
@@ -17,19 +22,36 @@ const usuarioTipoSchema = z.union([
   z.literal('MODERADOR')
 ])
 
+const usuarioStatusSchema = z.union([
+  z.literal('ATIVO'),
+  z.literal('DESATIVADO'),
+  z.literal('PENDENTE'),
+  z.literal('BLOQUEADO')
+])
+
+const usuarioGeneroSchema = z.union([
+  z.literal('MASCULINO'),
+  z.literal('FEMININO')
+])
+
 const updateUsuarioSchema = z.object({
   id: z.string().uuid(),
   nome: z.string(),
   tipo: usuarioTipoSchema,
-  email: z.string().email(),
-  telefone: z.string()
+  email: z.string().email(), 
+  telefone: z.string(),
+  status: usuarioStatusSchema,
+  cpf: z.string().min(11).max(11),
+  dataNascimento: z.coerce.date(),
+  genero: usuarioGeneroSchema,
+  senha: z.string().min(6)
 })
 
 class UpdateUsuarioUseCase {
   constructor (private readonly usuarioRepository: IUsuariosRepository) { }
 
-  async execute({id, nome, tipo, email, telefone}: IRequest):Promise<Usuario | null> {
-    updateUsuarioSchema.parse({id, nome, tipo, email, telefone})
+  async execute({id, nome, tipo, email, telefone, status, cpf, dataNascimento, genero, senha}: IRequest):Promise<Usuario | null> {
+    updateUsuarioSchema.parse({id, nome, tipo, email, telefone, status, cpf, dataNascimento, genero, senha})
 
     const usuarioExist = await this.usuarioRepository.getById(id)
     if (!usuarioExist) throw new Error("Usuario nao existe")
